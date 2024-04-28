d01d48bd1d71334fbbb734fe0e45b29008d1b759
shafaet.hossain@g.bracu.ac.bd <shafaet.hossain@g.bracu.ac.bd>
2024-04-28 18:49:14 +0600

    create paintings server side done

--- index.js
+++ index.js
@@ -32,7 +32,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    // consyt artsCollection =client.db('art')
+    await client.connect();
+
+
+    app.post('/addpaintings', async(req,res) =>{
+      const newPaintings =req.body;
+      console.log(newPaintings);
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,7 +50,7 @@ async function run() {
     // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.dir); 
 
 
 
